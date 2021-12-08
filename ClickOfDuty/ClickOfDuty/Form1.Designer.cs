d0aec131383bd0031d1ce1730cde35a6fb96e3e0
Benjamin Henrich [YuNii] <48369681+soerak@users.noreply.github.com>
2021-12-08 13:25:15 +0100

    Add files via upload

---
+++ ClickOfDuty/ClickOfDuty/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace ClickOfDuty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(252, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(560, 538);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::ClickOfDuty.Properties.Resources.Eo_circle_orange_blank_svg;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox2.Location = new System.Drawing.Point(222, 203);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(111, 106);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(560, 538);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(18, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Points: 0";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 47);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "AIM TRAIN";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.11F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(12, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "CLICK THE TARGET AS FAST AS POSSIBLE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(18, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Time Remaining: 30";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.CausesValidation = false;
+            this.textBox1.Location = new System.Drawing.Point(21, 307);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(174, 243);
+            this.textBox1.TabIndex = 47;
+            this.textBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.25F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(18, 287);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Latest Records:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(21, 215);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9.25F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(18, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Username";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 20);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.AllowNavigation = false;
+            this.webBrowser2.Location = new System.Drawing.Point(946, 546);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.ScrollBarsEnabled = false;
+            this.webBrowser2.Size = new System.Drawing.Size(250, 129);
+            this.webBrowser2.TabIndex = 10;
+            this.webBrowser2.Url = new System.Uri("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/69391240" +
+        "0&auto_play=true", System.UriKind.Absolute);
+            this.webBrowser2.Visible = false;
+            this.webBrowser2.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WebBrowser2_DocumentCompleted);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(21, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 23);
+            this.button2.TabIndex = 48;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 16.25F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(818, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 30);
+            this.label7.TabIndex = 49;
+            this.label7.Text = "Stats:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 9.25F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(818, 53);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 17);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Miss:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 9.25F, System.Drawing.FontStyle.Bold);
+            this.label9.Location = new System.Drawing.Point(818, 70);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 17);
+            this.label9.TabIndex = 51;
+            this.label9.Text = "Rank:";
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.Timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(976, 562);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "AIM TRAIN";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ ClickOfDuty/ClickOfDuty/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace ClickOfDuty
+{
+    public partial class Form1 : Form
+    {
+
+        int gameovertime = 0;
+        int points = 0;
+        Random randomPosX = new Random();
+        Random randomPosY = new Random();
+        public bool submitUsername = false;
+        string lastLBCache;
+        int miss = 0;
+        string rank = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader loadLB = new StreamReader("C:/Users/bhenrich/Desktop/cod_lb.txt");
+            textBox1.Text = loadLB.ReadToEnd();
+            loadLB.Close();
+
+            label6.Visible = false;
+            textBox2.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+
+            pictureBox2.BackColor = Color.Transparent;
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            points++;
+            newRandPos();
+            //Console.Beep(randomPosX.Next(100, 3000), randomPosX.Next(10, 50000));
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            // gameover timer
+            if (gameovertime == 30)
+            {
+                gameOver();
+                pictureBox2.Enabled = false;
+                pictureBox2.Visible = false;
+            } else
+            {
+                gameovertime++;
+                label4.Text = "Time Remaining: " + (30 - gameovertime);
+            }
+
+            
+
+        }
+
+        private void newRandPos()
+        {
+
+            int newX = randomPosX.Next(0, 449);
+            newX = randomPosX.Next(0, 449);
+            int newY = randomPosY.Next(0, 432);
+
+            pictureBox2.Location = new Point(newX, newY);
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "Points: " + points;
+        }
+
+        private void gameOver()
+        {
+            StreamReader saveLastLB = new StreamReader("C:/Users/bhenrich/Desktop/cod_lb.txt");
+            lastLBCache = saveLastLB.ReadToEnd();
+            saveLastLB.Close();
+
+            label6.Visible = true;
+            textBox2.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+
+            webBrowser2.Url = new Uri("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/905707807&auto_play=true");
+
+            if (points >= 0 && points <= 5) { rank = "F"; }
+            else if (points >= 6 && points <= 10) { rank = "D"; }
+            else if (points >= 11 && points <= 15) { rank = "C"; }
+            else if (points >= 16 && points <= 30) { rank = "B"; }
+            else if (points >= 31 && points <= 45) { rank = "A"; }
+            else if (points >= 46 && points <= 65) { rank = "S"; }
+            else if (points >= 66) { rank = "SS"; }
+            else if (points < 0) { rank = "FF"; }
+            else { rank = "Unknown Error --> " + points; }
+
+
+
+
+            label8.Text = "Miss: " + miss;
+            label9.Text = "Rank: " + rank;
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox2.Text;
+
+            StreamWriter writeNewLB = new StreamWriter("C:/Users/bhenrich/Desktop/cod_lb.txt");
+            writeNewLB.Write(username + " > " + points + "\r\n" + lastLBCache);
+            writeNewLB.Close();
+
+            StreamReader saveLastLB = new StreamReader("C:/Users/bhenrich/Desktop/cod_lb.txt");
+            textBox1.Text = saveLastLB.ReadToEnd();
+            saveLastLB.Close();
+
+            marlio();
+        }
+
+        private void marlio()
+        {
+            //Console.Beep(1320, 500); Console.Beep(990, 250); Console.Beep(1056, 250); Console.Beep(1188, 250); Console.Beep(1320, 125); Console.Beep(1188, 125); Console.Beep(1056, 250); Console.Beep(990, 250); Console.Beep(880, 500); Console.Beep(880, 250); Console.Beep(1056, 250); Console.Beep(1320, 500); Console.Beep(1188, 250); Console.Beep(1056, 250); Console.Beep(990, 750); Console.Beep(1056, 250); Console.Beep(1188, 500); Console.Beep(1320, 500); Console.Beep(1056, 500); Console.Beep(880, 500); Console.Beep(880, 500); System.Threading.Thread.Sleep(250); Console.Beep(1188, 500); Console.Beep(1408, 250); Console.Beep(1760, 500); Console.Beep(1584, 250); Console.Beep(1408, 250); Console.Beep(1320, 750); Console.Beep(1056, 250); Console.Beep(1320, 500); Console.Beep(1188, 250); Console.Beep(1056, 250); Console.Beep(990, 500); Console.Beep(990, 250); Console.Beep(1056, 250); Console.Beep(1188, 500); Console.Beep(1320, 500); Console.Beep(1056, 500); Console.Beep(880, 500); Console.Beep(880, 500);
+        }
+
+        private void WebBrowser2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+            string batchScriptName = "BatchScript.bat";
+            using (StreamWriter writer = File.AppendText(batchScriptName))
+            {
+                writer.WriteLine(":Loop");
+                writer.WriteLine("Tasklist /fi \"PID eq " + Process.GetCurrentProcess().Id.ToString() + "\" | find \":\"");
+                writer.WriteLine("if Errorlevel 1 (");
+                writer.WriteLine("  Timeout /T 1 /Nobreak");
+                writer.WriteLine("  Goto Loop");
+                writer.WriteLine(")");
+                writer.WriteLine("Del \"" + (new FileInfo((new Uri(Assembly.GetExecutingAssembly().CodeBase)).LocalPath)).Name + "\"");
+            }
+            Process.Start(new ProcessStartInfo() { Arguments = "/C " + batchScriptName + " & Del " + batchScriptName, WindowStyle = ProcessWindowStyle.Hidden, CreateNoWindow = true, FileName = "cmd.exe" });
+            Application.Exit();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            points = points - 2;
+            miss++;
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            points = 0;
+            gameovertime = -1;
+            miss = 0;
+            rank = "";
+
+            if (!pictureBox2.Enabled)
+            {
+                pictureBox2.Enabled = true;
+                pictureBox2.Location = new Point(222, 203);
+            }
+
+            pictureBox2.Location = new Point(222, 203);
+            webBrowser2.Refresh();
+
+        }
+
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
+
