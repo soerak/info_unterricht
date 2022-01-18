200cde802778460ccf2bf20deef7ec2864951501
Benjamin Henrich [YuNii] <48369681+soerak@users.noreply.github.com>
2022-01-18 15:59:55 +0100

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // VARS
+        Random random = new Random();
+        BOI[] User = new BOI[1];
+        string[] listOfNames = new string[] { "Jeremy", "Oleksandr", "Jacob", "Barak", "Lisa", "Marcel" };
+        string[] listOfLastNames = new string[] { "Roth", "Seeliger", "Lamar", "Benrivh", "Kostyliev", "Karlsruhe" };
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*   GENERATE VALUES   */
+            string a = listOfNames[random.Next(0, 5)];
+            string b = listOfLastNames[random.Next(0, 5)];
+            int randKN = random.Next(11111111, 99999999);
+            int randPin = random.Next(1111, 9999);
+            float randGH = random.Next(-1000, 10000);
+            int randSteal = random.Next(0, 2);
+            Console.WriteLine(randSteal);
+            bool stealed = false;
+            if (randSteal == 0) { stealed = true; } else if (randSteal == 1) { stealed = false; }
+            
+            /*   GENERATE USER USING VALUES   */
+            User[0] = new BOI(a + " " + b, randKN, randPin, randGH, stealed);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            User[0].printUser(textBox1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // einzahlen
+
+            float templast = User[0].Guthaben;
+            float tmpbetrag = float.Parse(textBox2.Text);
+            User[0].Guthaben = User[0].Guthaben + tmpbetrag;
+            textBox1.Text +=
+                "\r\n" + "------- KONTOÄNDERUNG --------" + "\r\n";
+            User[0].printUser(textBox1);
+
+            // logs
+            textBox3.Text +=
+                /* GUTHABEN */ 
+                "GUTHABEN:  \r\n"       + 
+                "    "                  + 
+                templast                + 
+                " --> "                 + 
+                User[0].Guthaben        + 
+                "\r\n \r\n"             ;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // auszahlen
+            float templast = User[0].Guthaben;
+            float tmpbetrag = float.Parse(textBox2.Text);
+            User[0].Guthaben = User[0].Guthaben - tmpbetrag;
+            textBox1.Text +=
+                "\r\n" + "------- KONTOÄNDERUNG --------" + "\r\n";
+            User[0].printUser(textBox1);
+
+            // logs
+            textBox3.Text +=
+                /* GUTHABEN */
+                "GUTHABEN:  \r\n" +
+                "    " +
+                templast +
+                " --> " +
+                User[0].Guthaben +
+                "\r\n \r\n";
+        }
+    }
+
+    public class BOI
+    {
+
+        public string Kunde;
+        public int Kundennummer;
+        public int Kundenpin;
+        public float Guthaben;
+        public bool KarteGeklaut;
+
+        public BOI(string kunde, int kundennummer, int kundenpin, float guthaben, bool kartegeklaut)
+        {
+
+            Kunde = kunde;
+            Kundennummer = kundennummer;
+            Kundenpin = kundenpin;
+            Guthaben = guthaben;
+            KarteGeklaut = kartegeklaut;
+
+        }
+
+        public void einzahlen(float betrag)
+        {
+            this.Guthaben += betrag;
+        }
+        public void auszahlen(float betrag)
+        {
+            this.Guthaben -= betrag;
+        }
+
+        public void printUser(TextBox txt)
+        {
+            txt.Text =
+                //"\r\n" + "\r\n" +
+                "Name >> " + this.Kunde + 
+                "\r\n" + 
+                "KundenNr >> " + this.Kundennummer.ToString() +
+                "\r\n" + 
+                "PIN >> " + this.Kundenpin.ToString() +
+                "\r\n" + 
+                "Guthaben >> " + this.Guthaben.ToString() +
+                "\r\n" +
+                "Karte Geklaut? >> " + this.KarteGeklaut.ToString();
+        }
+
+    }
+
+
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+
+        Konto myKonto = new Konto(6487, 6942, 764372);
+        User myUser = new User("Jacob", "Seeliger", 69429);
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt64(textBox1.Text) == myKonto.kontonummer && Convert.ToInt16(textBox2.Text) == myKonto.pin)
+            {
+                loadLogin();
+            }
+        }
+
+        void loadLogin()
+        {
+            Form1 form = new Form1();
+            form.Show();
+            this.Visible = false;
+        }
+    }
+
+    public class Konto
+    {
+        public float guthaben;
+        public int pin;
+        public int kontonummer;
+
+        public Konto(float _guthaben, int _pin, int _kontonummer)
+        {
+            guthaben = _guthaben;
+            pin = _pin;
+            kontonummer = _kontonummer;
+        }
+
+    }
+
+    public class User
+    {
+        public string vorname;
+        public string nachname;
+        public int kundennummer;
+
+        public User(string _vorname, string _nachname, int _kundennummer)
+        {
+            vorname     = _vorname;
+            nachname    = _nachname;
+            kundennummer = _kundennummer;
+        }
+    }
+}
