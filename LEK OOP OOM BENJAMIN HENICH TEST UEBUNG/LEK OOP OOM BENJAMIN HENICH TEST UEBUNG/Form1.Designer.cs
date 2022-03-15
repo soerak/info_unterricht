f7d38f34db1bda58f9bd121518ec6f5525e52141
Benjamin Henrich [YuNii] <48369681+soerak@users.noreply.github.com>
2022-03-15 14:05:25 +0100

    Add files via upload

---
+++ LEK OOP OOM BENJAMIN HENICH TEST UEBUNG/LEK OOP OOM BENJAMIN HENICH TEST UEBUNG/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace LEK_OOP_OOM_BENJAMIN_HENICH_TEST_UEBUNG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(372, 509);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "OUTPUT";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "PRINT ALL USERDATA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(390, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "PRINT ALL ROOMDATA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(390, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "PRINT ALL DATA";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::LEK_OOP_OOM_BENJAMIN_HENICH_TEST_UEBUNG.Properties.Resources.index;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(407, 138);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 387);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 557);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LEK OOP OOM BENJAMIN HENICH TEST UEBUNG/LEK OOP OOM BENJAMIN HENICH TEST UEBUNG/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LEK_OOP_OOM_BENJAMIN_HENICH_TEST_UEBUNG
+{
+    public partial class Form1 : Form
+    {
+
+        List<SuS> susListB = new List<SuS>();
+        List<SuS> susListE = new List<SuS>();
+        List<Raum> raumList = new List<Raum>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // CREATE ROOMS
+            raumList.Add(new Raum("01a", 30, 15, "Biologie"));
+            raumList.Add(new Raum("01e", 21, 50, "English"));
+
+            //CREATE BIO STUDENTS
+            susListB.Add(new SuS("Susi Sauer", "123456", "1.0"));
+            susListB.Add(new SuS("Kevin Schmidt", "geheim", "4.6"));
+            susListB.Add(new SuS("Chantal Müller", "chantal", "2.0"));
+
+            //CREATE ENG STUDENTS
+            susListE.Add(new SuS("Jeremy Vogel", "passwort", "4.0"));
+            susListE.Add(new SuS("Jennifer Schmidt", "geheim", "3.3"));
+            susListE.Add(new SuS("Justin Müller", "justin", "1.0"));
+
+            // APPEND SUS TO ROOMS
+            foreach (var sus in susListB) { raumList[0].addSus(sus); }
+            foreach (var sus in susListE) { raumList[1].addSus(sus); }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // PRINT ALL USERDATA
+            textBox1.Text = "";
+
+            foreach (SuS sus in susListB)
+            {
+                textBox1.Text +=
+                    "USRNAME: " +
+                    sus.getVar(0) +
+                    "\r\nGRADE: " +
+                    sus.getVar(2) +
+                    "\r\n\r\n";
+            }
+            foreach (SuS sus in susListE)
+            {
+                textBox1.Text +=
+                    "USRNAME: " +
+                    sus.getVar(0) +
+                    "\r\nGRADE: " +
+                    sus.getVar(2) +
+                    "\r\n\r\n";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            // PRINT ALL ROOMDATA
+            foreach (var room in raumList)
+            {
+                room.outputRaumData(textBox1);
+                textBox1.Text += "\r\n\r\n";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            raumList[0].outputRaumData(textBox1);
+            textBox1.Text += "AVERAGE GRADE:\t\t" + Convert.ToString(raumList[0].ReturnAverageGrade()) + "\r\n";
+            textBox1.Text += "SCHÜLER IN BIO-RAUM:\r\n";
+            foreach (var sus in susListB)
+            {
+                textBox1.Text +=
+                    "\tName:\t" + sus.getVar(0) + "\r\n" +
+                    "\tPWD:\t" + sus.getVar(1) + "\r\n" +
+                    "\tNote:\t" + sus.getVar(2) + "\r\n\r\n";
+            }
+            
+
+            textBox1.Text += "\r\n\r\n";
+
+            raumList[0].outputRaumData(textBox1);
+            textBox1.Text += "AVERAGE GRADE:\t\t" + Convert.ToString(raumList[1].ReturnAverageGrade()) + "\r\n";
+            textBox1.Text += "SCHÜLER IN ENG-RAUM:\r\n";
+            foreach (var sus in susListB)
+            {
+                textBox1.Text +=
+                    "\tName:\t" + sus.getVar(0) + "\r\n" +
+                    "\tPWD:\t" + sus.getVar(1) + "\r\n" +
+                    "\tNote:\t" + sus.getVar(2) + "\r\n\r\n";
+            }
+        }
+    }
+}
